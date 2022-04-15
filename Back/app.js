29119f0261a4e8ce332372102a92382dd0f97dd7
Aqlice <camille.jumptower@gmail.com>
2022-04-15 17:58:22 +0200

    push p6

--- Back/app.js
+++ Back/app.js
@@ -4,8 +4,9 @@ const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 const bodyParser = require('body-parser')
 const path = require('path');
+require("dotenv").config();
 
-mongoose.connect('mongodb+srv://Aqlice:jKa55WLwQSOGWoOo@cluster0.yvh8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
